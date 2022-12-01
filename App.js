7cc943c95de4a9d06b766c87c4dc269b0a488d15
sathish19961412 <sathishfreelanc5@gmail.com>
2022-12-01 15:31:08 +0530

    First Reactnative App

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Welcome To Shankar </Text>
+      <Text style={styles.text}>Welcome To Sathish</Text>
+      <Text style={styles.text}>Welcome To Ramaya</Text>
+      <StatusBar />
     </View>
+ 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightslategray',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
   },
 });
